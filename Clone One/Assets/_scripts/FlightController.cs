2959517c276383865929b555bd275de1bc9dc9ae
Daynavos <dayna22vos@gmail.com>
2025-07-30 08:55:06 +0200

    thrust

--- Clone One/Assets/_scripts/FlightController.cs
+++ Clone One/Assets/_scripts/FlightController.cs
@@ -10,15 +10,15 @@ public class FlightController : MonoBehaviour
     private InputAction _upThrust;
     private InputAction _downThrust;
     
-    private Vector2 _moveValue;
-    private Vector2 _lookValue;
-    private float _upThrustInput;
-    private float _downThrustInput;
+    private Vector2 _moveVector;
+    private Vector2 _lookVector;
+    private float _upThrustInputFloat;
+    private float _downThrustInputFloat;
 
-    [SerializeField] private float moveAmount = 5;
-    [SerializeField] private float lookAmount = 5;
-    [SerializeField] private float upThrustInput = 5;
-    [SerializeField] private float downThrustInput = 5;
+    [SerializeField] private float moveMultiplier = 5;
+    [SerializeField] private float lookMultiplier = 5;
+    [SerializeField] private float upThrustMultiplier = 2;
+    [SerializeField] private float downThrustMultiplier = 2;
     
     private Rigidbody _rigidbody;
 
@@ -42,20 +42,34 @@ public class FlightController : MonoBehaviour
 
     void Update()
     {
-        _moveValue = _moveAction.ReadValue<Vector2>();
-        _lookValue = _lookAction.ReadValue<Vector2>();
-        
+        _moveVector = _moveAction.ReadValue<Vector2>();
+        _lookVector = _lookAction.ReadValue<Vector2>();
+        _upThrustInputFloat = _upThrust.ReadValue<float>();
+        _downThrustInputFloat = _downThrust.ReadValue<float>();
     }
 
     void FixedUpdate()
     {
         MovingBasic();
         Looking();
+        UpThrust();
+        DownThrust();
     }
 
     private void MovingBasic()
     {
-        _rigidbody.MovePosition(_rigidbody.position + transform.forward * _moveValue.y );
+        _rigidbody.MovePosition(_rigidbody.position + transform.forward * _moveVector.y + transform.right * _moveVector.x );
+    }
+
+    private void UpThrust()
+    {
+        _rigidbody.MovePosition(_rigidbody.position + transform.up * (_upThrustInputFloat * upThrustMultiplier));
+    }
+
+    private void DownThrust()
+    {
+        _rigidbody.MovePosition(_rigidbody.position + transform.up * (_downThrustInputFloat * downThrustMultiplier * -1));
+
     }
 
     private void Looking()
